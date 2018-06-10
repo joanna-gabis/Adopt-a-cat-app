95e355f19fcb1959cb4c9da606883a041b4bb6ad
joanna-gabis <joanna.gabis@gmail.com>
2018-06-10 20:28:21 +0200

    added pics

--- js/app.jsx
+++ js/app.jsx
@@ -2,25 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../sass/style.scss';
 
-var kitties = [
- {category: "male", age: "4", likesKids: true, name: "Fidel Catstro"},
- {category: "male", age: "9", likesKids: true, name: "Hairy Potter"},
- {category: "male", age: "2", likesKids: false, name: "Grumpy"},
- {category: "male", age: "3", likesKids: true, name: "Ramzes"},
- {category: "male", age: "8", likesKids: false, name: "Gucio"},
- {category: "male", age: "5", likesKids: true, name: "Pisak"},
- {category: "female", age: "1", likesKids: true, name: "Jude Paw"},
- {category: "female", age: "2", likesKids: false, name: "Lucifurr"},
- {category: "female", age: "3", likesKids: true, name: "Meowly Cyrus"},
- {category: "female", age: "5", likesKids: true, name: "Klaudyna"},
- {category: "female", age: "4", likesKids: false, name: "Kicia"},
- {category: "female", age: "1", likesKids: true, name: "Mizia"},
- {category: "female", age: "2", likesKids: true, name: "Fizia"},
- {category: "female", age: "7", likesKids: false, name: "Ramona"},
- {category: "female", age: "6", likesKids: true, name: "Puszka"},
+var puppies = [
+ {category: "male", age: "5", likesKids: true, name: "Dobby", pic: './images/dog1.jpg'},
+ {category: "male", age: "4", likesKids: true, name: "Harry", pic: './images/dog2.jpeg'},
+ {category: "male", age: "2", likesKids: false, name: "Grumpy", pic: './images/dog3.jpg'},
+ {category: "male", age: "3", likesKids: true, name: "Ramzes", pic: './images/dog4.jpg'},
+ {category: "male", age: "8", likesKids: false, name: "Rex", pic: './images/dog5.jpg'},
+ {category: "male", age: "5", likesKids: true, name: "Pisak", pic: './images/dog6.jpg'},
+ {category: "female", age: "3", likesKids: true, name: "Paw", pic: './images/dog7.jpeg'},
+ {category: "female", age: "2", likesKids: false, name: "The Dog", pic: './images/dog1.jpg'},
+ {category: "female", age: "14", likesKids: true, name: "Simba", pic: './images/dog2.jpeg'},
+ {category: "female", age: "5", likesKids: true, name: "Claudette", pic: './images/dog3.jpg'},
+ {category: "female", age: "4", likesKids: false, name: "Regina", pic: './images/dog4.jpg'},
+ {category: "female", age: "1", likesKids: true, name: "Chap", pic: './images/dog1.jpg'},
+ {category: "female", age: "2", likesKids: true, name: "Freezy", pic: './images/dog6.jpg'},
+ {category: "female", age: "7", likesKids: false, name: "Ramona", pic: './images/dog4.jpg'},
+ {category: "female", age: "6", likesKids: true, name: "Punta", pic: './images/dog5.jpg'},
 ];
 
-class CatCategoryRow extends React.Component {
+class DogCategoryRow extends React.Component {
   render() {
     return <tr className='category'>
       <td></td>
@@ -32,20 +32,19 @@ class CatCategoryRow extends React.Component {
   }
 }
 
-class CatRow extends React.Component {
+class DogRow extends React.Component {
   render() {
-    let {kitty} = this.props;
-    //let name = kitty.likesKids ? kitty.name : <span style={{color: 'red'}}> {kitty.name} </span>;
-    let name = kitty.name;
+    let {puppy} = this.props;
+    let name = puppy.name;
     return <tr className='table-row'>
-      <td className='center'><img className='catPic' src='./images/cat.png'/></td>
-      <td><a href='#'>{name}</a> {!kitty.likesKids && <span className='tooltip'><img data-tooltip='this cat does not like kids' src='./images/kid.png'/></span>}</td>
-      <td className='center'>{kitty.age}</td>
+      <td className='center'><img className='dogPic' src={puppy.pic}/></td>
+      <td><a href='#'>{name}</a> {!puppy.likesKids && <span className='tooltip'><img data-tooltip='this dog does not like kids' src='./images/kid.png'/></span>}</td>
+      <td className='center'>{puppy.age}</td>
     </tr>;
   }
 }
 
-class CatTable extends React.Component {
+class DogTable extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -54,29 +53,29 @@ class CatTable extends React.Component {
     let lastCategory = null;
     const {filterText, doesLikeKids} = this.props;
 
-    this.props.kitties.forEach((elem) => {
+    this.props.puppies.forEach((elem) => {
       if (elem.category !== lastCategory) {
         let capitalizeCategory = elem.category.charAt(0).toUpperCase() + elem.category.substr(1);
-        rows.push(<CatCategoryRow category={capitalizeCategory}  key={elem.category}/>);
+        rows.push(<DogCategoryRow category={capitalizeCategory}  key={elem.category}/>);
       }
 
 // *** function to filter array by input value and chekckbox state *** //
       if(filterText==='') {
         if(doesLikeKids) {
           if(elem.likesKids) {
-            rows.push(<CatRow kitty={elem} key={elem.name} likesKids={elem.likesKids}/>);
+            rows.push(<DogRow puppy={elem} key={elem.name} likesKids={elem.likesKids}/>);
           };
         } else {
-          rows.push(<CatRow kitty={elem} key={elem.name} likesKids={elem.likesKids}/>);
+          rows.push(<DogRow puppy={elem} key={elem.name} likesKids={elem.likesKids}/>);
         }
       } else if(filterText !== '') {
         if(doesLikeKids) {
           if(elem.name.toLowerCase().includes(filterText) && elem.likesKids) {
-            rows.push(<CatRow kitty={elem} key={elem.name} likesKids={elem.likesKids}/>);
+            rows.push(<DogRow puppy={elem} key={elem.name} likesKids={elem.likesKids}/>);
           }
         } else if(!doesLikeKids) {
           if(elem.name.toLowerCase().includes(filterText)) {
-            rows.push(<CatRow kitty={elem} key={elem.name} likesKids={elem.likesKids}/>);
+            rows.push(<DogRow puppy={elem} key={elem.name} likesKids={elem.likesKids}/>);
           }
         }
       }
@@ -116,10 +115,10 @@ class SearchBar extends React.Component {
 
   render() {
     return <form className='searchbar'>
-      <input type="text" name='searchBar' placeholder="Search by cat's name" onChange={e=>this.handleInputChange(e)}/>
+      <input type="text" name='searchBar' placeholder="Search by dog's name" onChange={e=>this.handleInputChange(e)}/>
             <div className='checkbox' onClick={e=>this.likesKids(e)}>
               {this.props.doesLikeKids ? <div className='checkboxBox'><img src='./images/tick.png'/></div> : <div className='checkboxBox'></div>}
-              <p className='checkboxDesc'>Only show kitties that like kids</p>
+              <p className='checkboxDesc'>Only show puppies that like kids</p>
             </div>
     </form>;
   }
@@ -128,7 +127,7 @@ class SearchBar extends React.Component {
 function MainHeader(props) {
   return <div className='main-header-container'>
     <p className='main-header'>
-      Adopt a cat
+      Adopt a dog
     </p>
 
   </div>
@@ -164,8 +163,8 @@ class App extends React.Component {
             likesKids={this.likesKids}
             changeInputValue={this.handleInputChange}
             />
-          <CatTable
-            kitties={this.props.kitties}
+          <DogTable
+            puppies={this.props.puppies}
             doesLikeKids={this.state.likesKids}
             filterText={this.state.filterText}
           />
@@ -175,6 +174,6 @@ class App extends React.Component {
 }
 
 ReactDOM.render(
-  <App kitties={kitties}/>,
+  <App puppies={puppies}/>,
   document.getElementById('app')
 );
